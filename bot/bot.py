5ec044a0207fba008b5a29a4c4d767aeff6dd13a
Ivan Kosovtsev <r.ivankos21@gmail.com>
2022-09-02 21:32:52 +0200

    Send message on ready

--- bot/bot.py
+++ bot/bot.py
@@ -8,6 +8,11 @@ import music
 import misc
 
 
+@client.event
+def on_ready():
+    dev_channel = client.get_channel(config.DEV_CHANNEL_ID)
+    await dev_channel.send("DIABLO Strike restarted and ready!")
+
 def main():
     client.run(config.DISCORD_BOT_TOKEN)
 
