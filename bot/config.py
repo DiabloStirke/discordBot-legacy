7183ef717e970cf5ba368aacee1acad9da11c400
Ivan Kosovtsev <i.kosovtsev@spotapp.ru>
2023-02-01 19:37:48 +0100

    Prepare config

--- bot/config.py
+++ bot/config.py
@@ -4,10 +4,13 @@ DATETIME_STR_FORMAT = '%y-%m-%d_%H:%M:%S'
 
 DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN', 'OTIzNzE4Mjg5NzQ4Nzg3MjEz.YcUFsg.vpdcbzez_hXfycU5QEVy3-WFVkc')
 
+# Default channel name for ?shift command
 DEATH_CHANNEL = os.environ.get('DEATH_CHANNEL', 'Víctima del DIABLO STRIKE')
 
-DEV_CHANNEL_ID = 923745656227655781
+# Commit message on start up config
+DEV_CHANNEL_ID = 1070411336732913714  # 923745656227655781
 LAST_COMMIT_MSG = os.environ.get('COMMIT_MESSAGE', None)
 
+# My Anime List anime info
 MAL_MAX_ANIMES = 19342
 MAL_ANIMES_PER_PAGE = 50
