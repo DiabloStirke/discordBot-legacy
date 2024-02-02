edb349dec33749bf1702a353e4c65299518dfebe
Ivan Kosovtsev <r.ivankos21@gmail.com>
2024-02-02 23:13:19 +0100

    Update CSS and implment Silksong News page

--- controlpanel/web/pages/silksong.py
+++ controlpanel/web/pages/silksong.py
@@ -1,5 +1,7 @@
-from flask import render_template, request, redirect, url_for, flash, Blueprint
+from flask import render_template, request, redirect, url_for, flash, Blueprint, current_app
+from flask import session
 from web.models.user import RoleEnum
+from web.models.silksong import SilksongNews
 from web.utils import ensure_session, ensure_role, get_main_context, tz_now, tz_fromiso, ordinal
 
 silksong = Blueprint('silksong', __name__)
@@ -13,6 +15,21 @@ def silksong_get():
     context = get_main_context()
     context['date'] = verbose_date
 
+    empty_news = {
+        'id': 'new',
+        'message': None,
+        'date': now,
+        'created_at': now,
+        'verbose_date': verbose_date,
+        'author': {
+            'avatar_url': session['user']['avatar_url'],
+            'username': session['user']['username']
+        }
+    }
+
+    silksong_news = SilksongNews.get_all(SilksongNews.date.desc(), SilksongNews.created_at.desc())
+    silksong_news.insert(0, empty_news)
+    context['news_list'] = silksong_news
     return render_template('silksong.html', **context), 200
 
 
@@ -24,6 +41,69 @@ def silksong_post():
     message = request_data.get('message', None)
     date = request_data.get('date', None)
     date = tz_fromiso(date)
-    print(message, date)
+
+    if not message:
+        flash('Please, avoid trying to create an empty Silksong News message.', 'error')
+        return redirect(url_for('silksong.silksong_get'))
+    elif not date:
+        flash('Please enter a valid date, we need to know when there were those news.', 'error')
+        return redirect(url_for('silksong.silksong_get'))
+    elif date.date() > tz_now().date():
+        flash('Please enter a date in the past, we cannot know the future.', 'error')
+        return redirect(url_for('silksong.silksong_get'))
+
+    silksong_news = SilksongNews.new(message=message, date=date, author_id=session['user']['id'])
+
+    if silksong_news:
+        flash('Silksong News message created successfully.', 'success')
+
+    else:
+        flash('Something went wrong while creating the Silksong News message.', 'error')
+
+    return redirect(url_for('silksong.silksong_get'))
+
+@silksong.route('/silksong/<int:silksong_news_id>/delete', methods=['POST'], endpoint='silksong_delete')
+@ensure_session
+@ensure_role(RoleEnum.ADMIN)
+def silksong_delete(silksong_news_id):
+    silksong_news = SilksongNews.get_by_id(silksong_news_id)
+    if silksong_news:
+        silksong_news.delete()
+        flash('Silksong News message deleted successfully.', 'success')
+    else:
+        flash('Invalid Silksong News ID', 'error')
+
+    return redirect(url_for('silksong.silksong_get'))
+
+
+@silksong.route('/silksong/<int:silksong_news_id>/edit', methods=['POST'], endpoint='silksong_edit')
+@ensure_session
+@ensure_role(RoleEnum.ADMIN)
+def silksong_edit(silksong_news_id):
+    request_data = request.form
+    message = request_data.get('message', None)
+    date = request_data.get('date', None)
+    date = tz_fromiso(date)
+    if not message:
+        flash('Please, avoid trying to save an empty Silksong News message.', 'error')
+        return redirect(url_for('silksong.silksong_get'))
+    elif not date:
+        flash('Please enter a valid date, we need to know when there were those news.', 'error')
+        return redirect(url_for('silksong.silksong_get'))
+    elif date.date() > tz_now().date():
+        flash('Please enter a date in the past, we cannot know the future.', 'error')
+        return redirect(url_for('silksong.silksong_get'))
+
+    silksong_news = SilksongNews.get_by_id(silksong_news_id)
+    current_app.logger.info(message)
+    current_app.logger.info(date)
+    current_app.logger.info(silksong_news)
+    if silksong_news:
+        silksong_news.message = message
+        silksong_news.date = date
+        silksong_news.save()
+        flash('Silksong News message edited successfully.', 'success')
+    else:
+        flash('Invalid Silksong News ID', 'error')
 
     return redirect(url_for('silksong.silksong_get'))
--- controlpanel/web/static/js/silksong.js
+++ controlpanel/web/static/js/silksong.js
@@ -21,27 +21,77 @@ function formatNewsDateMessage(date){
 
 $(function() {
 
-    // Paste clipboard data as plain text
-    $(document).on('paste', 'p[contenteditable]', function(e) {
-        e.preventDefault();
-        const clipboardData = e.clipboardData || e.originalEvent.clipboardData || window.clipboardData;
-        const text = clipboardData.getData('text/plain');
-        const selection = window.getSelection()
-        range = selection.getRangeAt(0);
-        range.deleteContents();
-        range.insertNode(document.createTextNode(text));
-        selection.collapseToEnd();
-        console.log(text);
-    });
 
+   { // Scope for variables. 
+        let char_limit = 1750;
+        
+        // Paste clipboard data as plain text
+        $(document).on('paste', 'p[contenteditable]', function(e) {
+            e.preventDefault();
+            const clipboardData = e.clipboardData || e.originalEvent.clipboardData || window.clipboardData;
+            const text = clipboardData.getData('text/plain');
+            const selection = window.getSelection()
+            range = selection.getRangeAt(0);
+            range.deleteContents();
+            range.insertNode(document.createTextNode(text));
+            selection.collapseToEnd();
+            console.log(text);
+
+            if ($(this).text().length >= char_limit) {
+                $(this).addClass('content-editable-error');
+                $(this).siblings('.content-editable-error-label').show();
+                const trimmed_text = $(this).text().substring(0, char_limit);
+                $(this).text(trimmed_text);
+            }
+
+        });
+        
+        // Prevent typing more than "char_limit" characters
+        $(document).on('keydown', 'p[contenteditable]', function(e) {
+            allowed_keys = [8, 16, 17, 37, 38, 39, 40, 46];
+            if ($(this).text().length >= char_limit && !allowed_keys.includes(e.keyCode)) {
+                e.preventDefault();
+                $(this).addClass('content-editable-error');
+                $(this).siblings('.content-editable-error-label').show();
+            }
+            else if (e.keyCode === 8 || e.keyCode === 46) {
+                $(this).removeClass('content-editable-error');
+                $(this).siblings('.content-editable-error-label').hide();
+
+            }
+        });
+
+        $(document).on('input', 'p[contenteditable]', function(e) {
+            if ($(this).text().length > char_limit) {
+                $(this).addClass('content-editable-error');
+                $(this).siblings('.content-editable-error-label').show();
+                const trimmed_text = $(this).text().substring(0, char_limit);
+                $(this).text(trimmed_text);
+            } else {
+                $(this).removeClass('content-editable-error');
+                $(this).siblings('.content-editable-error-label').hide();
+            }
+        });
+
+    }
     // Preview button click handler
-    $(".dc-embed-preview-btn").click(function(e) {
+    $(".dc-embed-preview-btn").on('click', function(e) {
         const preview_btn_id = "#" + this.id;
         const embed_id = "#" + this.id.replace(/-preview-btn$/, '');
         const edit_btn_id = embed_id + '-edit-btn';
         const date_btn_id = embed_id + '-date-btn';
 
+        if ($(embed_id).prop('contenteditable') === "false") {
+            return;
+        }
+
         let html = $(embed_id).html()
+        // html holds, well, the html. The first time it's called, it holds the text stored in the database.
+        // database stores the text with \n as line breaks, but html uses <br> as line breaks.
+        // So, we need to replace \n with <br> to display the text correctly.
+        html = html.replaceAll('\r\n', '\n').replaceAll('\r', '\n').replaceAll('\n', '<br>');
+        // However, original_data holds the text as it is stored in the database, so we need to replace <br> with \n
+        // Seems kinda redundant, but it's necessary. Although there might be a better way to do this.
         let original_data = html.replace(/<br>$/, '').replaceAll(/<br>/g, '\n');
         original_data = removeTags(original_data);
         
@@ -50,13 +100,13 @@ $(function() {
         
         $(embed_id).prop('contenteditable', false).attr('data-original', original_data);
         $(embed_id).html(html);
-        $(edit_btn_id).prop('disabled', false);
-        $(preview_btn_id).prop('disabled', true);
+        $(edit_btn_id).show()
+        $(preview_btn_id).hide();
         $(date_btn_id).invisible();
     });
 
     // Edit button click handler
-    $(".dc-embed-edit-btn").click(function(e) {
+    $(".dc-embed-edit-btn").on('click', function(e) {
         const edit_btn_id = "#" + this.id;
         const embed_id = "#" + this.id.replace(/-edit-btn$/, '');
         const preview_btn_id = embed_id + '-preview-btn';
@@ -67,73 +117,111 @@ $(function() {
         const html = original_data.replaceAll(/\n/g, '<br>');
 
         $(embed_id).html(html).prop('contenteditable', true);
-        $(preview_btn_id).prop('disabled', false);
-        $(edit_btn_id).prop('disabled', true);
+        $(preview_btn_id).show();
+        $(edit_btn_id).hide();
         $(date_btn_id).visible();
     });
 
-    $(".dc-embed-save-btn").click(function(e) {
+    $(".dc-embed-edit-mode-btn").on('click', function(e) {
+        const embed_id = "#" + this.id.replace(/-edit-mode-btn$/, '');
+        const edit_btn_id = embed_id + '-edit-btn';
+        $(edit_btn_id).parents('.dc-btn-container').show();
+        $(this).parents('.dc-embed-drawer').remove(); // yup, one-way edit mode (you can't go back to preview mode)
+        $(edit_btn_id).trigger('click');
+        $(embed_id).trigger('focus');
+        // A lot of things going on just to set the cursor at the end of the text
+        const range = document.createRange();
+        const sel = window.getSelection();
+        const embed = $(embed_id)[0];
+        range.setStart(embed, embed.childNodes.length);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    });
+
+    // Save button click handler
+    $(".dc-embed-save-btn").on('click', function(e) {
         const embed_id = "#" + this.id.replace(/-save-btn$/, '');
         const preview_btn_id = embed_id + '-preview-btn';
-        if (!$(preview_btn_id).prop('disabled')) {
-            $(preview_btn_id).click();
+        if ($(preview_btn_id).is(':visible')) {
+            $(preview_btn_id).trigger('click');
         }
-        const message = $(embed_id).attr('data-original');
+        let message = $(embed_id).attr('data-original');
         const date = $(embed_id).attr('data-date') || new Date().toISOString();
+        const id = $(this).attr('data-news-id');
+        message = message.replaceAll('"', "'");
+
+        let action = "/silksong"
+        if (id != 'new') {
+            action += "/" + id + "/edit";
+        }
         // Dirty hack to send the data to the server with a POST request and redirect
         // by creating a form and submitting it
-        const form = $('<form action="/silksong" method="POST">' +
+        const form = $('<form action="'+ action +'" method="POST">' +
             '<input type="hidden" name="message" value="' + message + '" />' +
             '<input type="hidden" name="date" value="' + date  + '" />' +
             '</form>'
         );
         $('body').append(form);
-        form.submit();
-    });
-    // Double click on message handler
-    $("p[contenteditable]").doubleClick(function(e) {
-        if (this.isContentEditable) return;
-        
-        $("#" + this.id + "-edit-btn").click();
+        form.trigger('submit');
     });
 
     // Attach date picker to date button
-    $('.dc-embed-date-btn').tempusDominus({
-        display: {
-            icons: {
-                type: 'icons',
-                time: 'bi bi-clock',
-                date: 'bi bi-calendar-week',
-                up: 'bi bi-arrow-up-circle',
-                down: 'bi bi-arrow-down-circle',
-                previous: 'bi bi-arrow-left-circle',
-                next: 'bi bi-arrow-right-circle',
-                today: 'bi bi-calendar-check',
-                clear: 'bi bi-trash',
-                close: 'bi bi-x-circle'
+    $('.dc-embed-date-btn').each( function(){
+        const picker = new tempusDominus.TempusDominus(this, {
+            display: {
+                icons: {
+                    type: 'icons',
+                    time: 'bi bi-clock',
+                    date: 'bi bi-calendar-week',
+                    up: 'bi bi-arrow-up-circle',
+                    down: 'bi bi-arrow-down-circle',
+                    previous: 'bi bi-arrow-left-circle',
+                    next: 'bi bi-arrow-right-circle',
+                    today: 'bi bi-calendar-check',
+                    clear: 'bi bi-trash',
+                    close: 'bi bi-x-circle'
+                },
+                components: {
+                    decades: false,
+                    clock: false,
+                    hours: false,
+                    minutes: false,
+                    seconds: false,
+                },
+                buttons: {
+                    today: true,
+                    close: true
+                },
+                theme: "dark"
             },
-            components: {
-                decades: false,
-                clock: false,
-                hours: false,
-                minutes: false,
-                seconds: false,
-            },
-            buttons: {
-                today: true,
-                close: true
-            },
-            theme: "dark"
-        },
-        restrictions: {
-            maxDate: getLastMomentOfToday()
+            restrictions: {
+                maxDate: getLastMomentOfToday()
+            }
+        })
+
+        const embed_id = "#" + this.id.replace(/-date-btn$/, '');
+
+        this.addEventListener(tempusDominus.Namespace.events.change, function(e) {
+            $(embed_id).attr('before-text', formatNewsDateMessage(e.detail.date));
+            $(embed_id).attr('data-date', e.detail.date.toISOString());
+        });
+
+        let date = $(embed_id).attr('data-date');
+        if (date) {
+           // date = new Date(date);
+            //$(embed_id).attr('before-text', formatNewsDateMessage(date));
+            picker.dates.setValue(new tempusDominus.DateTime(date));
         }
     });
 
-    // Date on date change handler
-    $('.dc-embed-date-btn').on(tempusDominus.Namespace.events.change, function(e) {
-        const embed_id = "#" + this.id.replace(/-date-btn$/, '');
-        $(embed_id).attr('before-text', formatNewsDateMessage(e.date));
-        $(embed_id).attr('data-date', e.date.toISOString());
+    $('p.dc-news-body').not('#dc-embed-new').each(function(){
+        $('#'+this.id+'-preview-btn').trigger('click');
+        $('#'+this.id+'-preview-btn').hide();
+        $('#'+this.id).parents('.news-item').show();
+    });
+
+    $('.dc-avatar').one('error', function(){
+        $(this).attr('src', '/rick-astley.png')
     });
 });
