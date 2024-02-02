c39f72ae5d3a459efa07e0bb0b1f5fd82838d465
Ivan Kosovtsev <r.ivankos21@gmail.com>
2024-02-02 23:10:44 +0100

    Add verbose_near_date filter and update Base class

--- controlpanel/web/__init__.py
+++ controlpanel/web/__init__.py
@@ -5,7 +5,7 @@ from web.models import *
 from web.pages import blueprints as page_blueprints
 from web.api import blueprints as api_blueprints
 from web.commands import blueprints as command_blueprints
-
+import datetime
 
 
 def create_app(config=None):
@@ -20,6 +20,23 @@ def create_app(config=None):
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
 
+    register_filters(app)
+
     db.init_app(app)
     migrate.init_app(app, db)
     return app
+
+
+def register_filters(app):
+    @app.template_filter('verbose_near_date')
+    def verbose_near_date_filter(d: datetime.datetime):
+        now = datetime.datetime.now().date()
+        date = d.date()
+        if date == now:
+            verbdate = 'Today'
+        elif date == now - datetime.timedelta(days=1):
+            verbdate = 'Yesterday'
+        else:
+            verbdate = d.strftime('%d/%m/%Y')
+
+        return verbdate + ' at ' + d.strftime('%I:%M %p')
--- controlpanel/web/db.py
+++ controlpanel/web/db.py
@@ -1,14 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import select
+from sqlalchemy.sql.selectable import Select
+from typing import Self, List
 
 
-class BaseModel(DeclarativeBase):
-    __abstract__ = True
-
+class Base(DeclarativeBase):
     ID_FIELD = 'id'
 
-    def update(self, ignore_null=False, **kwargs):
+    def update(self, ignore_null=False, **kwargs) -> None:
         if ignore_null:
             for key, value in kwargs.items():
                 if value is None:
@@ -17,30 +18,48 @@ class BaseModel(DeclarativeBase):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+        self.save()
 
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self):
+    def delete(self) -> None:
         db.session.delete(self)
         db.session.commit()
 
-    @classmethod
-    def get_all(cls):
-        return db.session.query(cls).all()
+    def save(self) -> None:
+        db.session.add(self)
+        db.session.commit()
 
     @classmethod
-    def get_by_id(cls, obj_id):
+    def get_all(cls, *order_by) -> List[Self]:
+        if order_by:
+            return cls.execute(cls.select.order_by(*order_by))
+        return cls.execute(cls.select)
+
+    @classmethod
+    def get_by_id(cls, obj_id) -> Self:
         filter = {cls.ID_FIELD: obj_id}
         return db.session.query(cls).filter_by(**filter).first()
 
     @classmethod
-    def new(cls, **kwargs):
+    def new(cls, **kwargs) -> Self:
         obj = cls(**kwargs)
         db.session.add(obj)
         db.session.commit()
         return obj
 
+    @classmethod
+    def filter_by(cls, expression) -> List[Self]:
+        return cls.execute(cls.select.where(expression))
 
-db = SQLAlchemy(model_class=BaseModel)
+    @classmethod
+    @property
+    def select(cls):
+        return select(cls)
+
+    @staticmethod
+    def execute(select_query) -> List[Self]:
+        rows = db.session.execute(select_query).all()
+        return [row[0] for row in rows]
+
+
+db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
